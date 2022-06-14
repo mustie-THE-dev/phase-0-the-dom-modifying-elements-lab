4eaacd655597517d44b71aa346aaa8b534e9f149
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-14 09:03:04 +0300

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const testVar = "h1";
 document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerHTML = "Mustaffins is the champion!";
+newHeader.innerHTML = "Mustaffinss is the champion!";
 newHeader.className = "victory";
